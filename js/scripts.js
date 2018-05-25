894e27422f13e55379674cbae62cc5ee522b685e
robbiesh <sherrard.rob@gmail.com>
2018-05-25 09:37:15 -0400

    anims

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,53 @@
 jQuery(document).ready(function() {
+
+    var pos = $(window).scrollTop();
+    setScreen(pos);
 });
 
 var trigger1 = $("#trigger1").offset().top - $(window).height();
 var trigger1_hit = false;
 
 $(window).scroll(function() {
-
-  var pos = $(window).scrollTop();
-
-  if (pos > trigger1) {
-
-     if (!trigger1_hit)  {
-       trigger1_hit = true;
-
-       // move to state 2
-
-       to2from1();
-
-       console.log('we hit trigger1');
-     }
-
-  } else {
-
-    if (trigger1_hit)  {
-      trigger1_hit = false;
-
-      // return to state 1
-
-      $(".image").fadeIn();
-
-      console.log('reset it');
-    }
-
-  }
-
+    var pos = $(window).scrollTop();
+    setScreen(pos);
 });
 
+function setScreen(pos) {
+
+    if (pos > trigger1) {
+
+       if (!trigger1_hit)  {
+         trigger1_hit = true;
+
+         // move to state 2
+
+         to2from1();
+
+         console.log('we hit trigger1');
+       }
+
+    } else {
+
+      if (trigger1_hit)  {
+        trigger1_hit = false;
+
+        // return to state 1
+
+        to1from2();
+
+        console.log('reset it');
+      }
+
+    }
+
+}
+
 function to2from1() {
-  $(".image").fadeOut();
+    $(".screen").removeClass("screen-1");
+    $(".screen").addClass("screen-2");
 }
 
 function to1from2() {
-  $(".image").fadeIn();
+    $(".screen").removeClass("screen-2");
+    $(".screen").addClass("screen-1");
 }
